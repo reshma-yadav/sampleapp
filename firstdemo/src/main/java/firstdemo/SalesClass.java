920a57dd4d9657e8b0a986604293c3f2f75f0520
Admin <Admin@Reshma-HP>
2021-08-17 10:53:45 +0530

    Display method updated in Sales class.

--- firstdemo/src/main/java/firstdemo/SalesClass.java
+++ firstdemo/src/main/java/firstdemo/SalesClass.java
@@ -4,7 +4,17 @@ public class SalesClass {
 
 	public void display() {
 		int[] arr= {1,2,3,4,5,6};
-		for(int num:arr)
-			System.out.println(num);
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+			System.out.println("Array Size:"+arr.length);
+			
+			
+		}
+			
+	}
+	
+	public void print() {
+		System.out.println("Print ALl Array Number:");
+		display();
 	}
 }
