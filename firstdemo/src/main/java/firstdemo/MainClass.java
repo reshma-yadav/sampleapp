55409c393caecf04d0ff1c4a6aa99c29213fc49d
Admin <Admin@Reshma-HP>
2021-08-17 12:26:41 +0530

    greetUser updated

--- firstdemo/src/main/java/firstdemo/MainClass.java
+++ firstdemo/src/main/java/firstdemo/MainClass.java
@@ -4,11 +4,11 @@ public class MainClass {
 
 	public void greetUser() {
 		
-		
-	}
-	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World1!");
+	}
+	public static void main(String[] args) {
+		
 	}
 
 }
