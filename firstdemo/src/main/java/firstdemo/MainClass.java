a241266475b5dfb01aa4fe395526f547ea6828d6
Admin <Admin@Reshma-HP>
2021-08-17 12:27:36 +0530

    greetUser Method invoked

--- firstdemo/src/main/java/firstdemo/MainClass.java
+++ firstdemo/src/main/java/firstdemo/MainClass.java
@@ -2,13 +2,13 @@ package firstdemo;
 
 public class MainClass {
 
-	public void greetUser() {
+	public static void greetUser() {
 		
 		System.out.println("Hello World!");
 		System.out.println("Hello World1!");
 	}
 	public static void main(String[] args) {
-		
+		greetUser();
 	}
 
 }
